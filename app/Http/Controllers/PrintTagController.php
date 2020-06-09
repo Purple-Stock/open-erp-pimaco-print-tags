e18a4fd049511e8b6b2ef9831afa503160ac6566
puppe1990 <matheus.puppe90@hotmail.com>
2020-06-08 21:08:04 -0300

    Remove harcode

--- app/Http/Controllers/PrintTagController.php
+++ app/Http/Controllers/PrintTagController.php
@@ -25,10 +25,9 @@ class PrintTagController extends Controller
     		for ($j = 0; $j <= $request["quantity"][$i] - 1; $j++) {
 				$tag = new Tag();
 				$tag->setPadding(3);
-				// $tag->setBorder(0.1);
 				$tag->img("https://17741.static.simplo7.net/static/17741/configuracao/logo_151893456531094.png")->setHeight(20)->setAlign('right');
 				$tag->qrcode($request["product_name"][$i])->setMargin(array(0,2,2,0))->br();
-				$tag->p('0001 - '.$request["product_name"][$i])->setSize(5)->br();
+				$tag->p($request["product_name"][$i])->setSize(5)->br();
 				$tag->p('R$:'.$request["price"][$i])->b()->setSize(5);
 				$pimaco->addTag($tag);
 			}
